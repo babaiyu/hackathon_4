2f9b0ed1c604aa353bac30fd335ab49e4268a9ae
Vinsensius <vinsensiusvp@gmail.com>
2021-07-04 11:48:59 +0700

    penambahan modul register

---
+++ frontend/src/login.php
@@ -0,0 +1,32 @@
+<!-- bradcam_area_start -->
+<div class="bradcam_area breadcam_bg">
+    <h3>Login</h3>
+</div>
+<!-- bradcam_area_end -->
+
+
+<!-- Start Align Area -->
+<div class="whole-wrap">
+    <div class="container box_1170">
+        <div class="section-top-border">
+            <div class="row">
+                <div class="col-lg-8 col-md-8">
+                    <!-- <h3 class="mb-30">Form Login</h3> -->
+                    <form action="#" id="formLogin" method="POST">
+                        <div class="mt-10">
+                            <input type="text" name="username" placeholder="First Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" required class="single-input">
+                        </div>
+                        <div class="mt-10">
+                            <input type="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required class="single-input">
+                        </div>
+                        <br>
+                        <a href="#" class="genric-btn primary-border">Primary</a>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Align Area -->
---
+++ frontend/src/register.php
@@ -0,0 +1,40 @@
+<!-- bradcam_area_start -->
+<div class="bradcam_area breadcam_bg">
+    <h3>Register</h3>
+</div>
+<!-- bradcam_area_end -->
+
+
+<!-- Start Align Area -->
+<div class="whole-wrap">
+    <div class="container box_1170">
+        <div class="section-top-border">
+            <div class="row">
+                <div class="col-lg-8 col-md-8">
+                    <!-- <h3 class="mb-30">Form Register</h3> -->
+                    <form action="#" id="formRegister" method="POST">
+                        <div class="mt-10">
+                            <input type="text" name="username" placeholder="First Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'" required class="single-input">
+                        </div>
+                        <div class="mt-10">
+                            <input type="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required class="single-input">
+                        </div>
+
+                        <div class="mt-10">
+                            <input type="text" name="name" placeholder="Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required class="single-input">
+                        </div>
+
+                        <div class="mt-10">
+                            <input type="number" name="no_hp" placeholder="Number Phone" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Number Phone'" required class="single-input">
+                        </div>
+                        <br>
+                        <a href="#" class="genric-btn primary-border">Primary</a>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Align Area -->
